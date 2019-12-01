1932b4eae73fd3b135a4a0046e317b03a3da8032
Helco <hermann.noll@hotmail.com>
2019-12-01 12:00:49 +0100

    Day 1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace day1
 {
     class Program
     {
+
+        static int FuelFor(int mass)
+        {
+            int totalFuel = 0;
+            int addFuel = (mass / 3) - 2;
+            do
+            {
+                totalFuel += addFuel;
+                addFuel = (addFuel / 3) - 2;
+            } while (addFuel > 0);
+            return totalFuel;
+        } 
+
+        static int ShipFuel() => File
+                .ReadAllLines("input.txt")
+                .Select(l => Convert.ToInt32(l))
+                .Select(FuelFor)
+                .Sum();
+        static void part2(string[] args)
+        {
+            Console.WriteLine("Test 3: " + FuelFor(100756) + " == 50346");
+
+            Console.WriteLine(ShipFuel());
+            Console.ReadKey(true);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            part2(args);
         }
     }
 }
