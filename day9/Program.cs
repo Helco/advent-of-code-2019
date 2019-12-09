51ae1f4f4eefc40376b4970c70d74f25934029b9
D067826 <hermann.justin.noll@sap.com>
2019-12-09 08:31:15 +0100

    Day 9

---
+++ day9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntCode2.tests(args);
+
+            Console.WriteLine("Part 1: ");
+            new IntCode2(IntCode2.Input).Run(1).PrintOutput();
+            new IntCode2(IntCode2.Input).Run(2).PrintOutput();
+
+            Console.WriteLine("done");
+            Console.ReadKey(true);
+        }
+    }
+}
