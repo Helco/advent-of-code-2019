16961b425b40719ddf801ec6b2d15e9e7c084286
Helco <hermann.noll@hotmail.com>
2020-12-03 19:53:21 +0100

    Day 10

--- day10/AsteroidMap.cs
+++ day10/AsteroidMap.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using UnityEngine;
 
 namespace day10
 {
     class AsteroidMap
     {
-        Vector2Int[] asteroids;
+        IVec2[] asteroids;
         int width, height;
 
         AsteroidMap(string map)
         {
-            var result = new List<Vector2Int>();
+            var result = new List<IVec2>();
             var lines = map.Split("\n").Select(l => l.Trim()).Where(l => l.Length > 0);
             int y = 0;
             foreach (var line in lines)
@@ -22,7 +21,7 @@ namespace day10
                 foreach (var c in line.ToArray())
                 {
                     if (c == '#')
-                        result.Add(new Vector2Int(x, y));
+                        result.Add(new IVec2(x, y));
                     x++;
                 }
                 y++;
@@ -33,9 +32,9 @@ namespace day10
             this.asteroids = result.ToArray();
         }
 
-        public bool IsInsideMap(Vector2Int v) => v.x >= 0 && v.y >= 0 && v.x < width && v.y < height;
+        public bool IsInsideMap(IVec2 v) => v.x >= 0 && v.y >= 0 && v.x < width && v.y < height;
 
-        public int? AsteroidAt(Vector2Int v)
+        public int? AsteroidAt(IVec2 v)
         { 
             for (int i = 0; i < asteroids.Length; i++)
             {
@@ -45,58 +44,134 @@ namespace day10
             return null;
         }
 
-        public IEnumerable<int> VisibleFrom(int rootI)
+        public IEnumerable<IVec2> Line(IVec2 source, IVec2 target)
         {
-            var visible = Enumerable.Range(0, asteroids.Length).ToHashSet();
-            for (int i = 0; i < asteroids.Length; i++)
+            var slope = (target - source).Normalized;
+            var cur = source;
+            do
             {
-                if (i == rootI)
-                    continue;
+                yield return cur;
+                cur += slope;
+            } while (cur != target && IsInsideMap(cur));
+        }
 
-                Vector2Int step = asteroids[i] - asteroids[rootI];
-                Vector2Int cur = asteroids[i] + step;
-                while (IsInsideMap(cur))
-                {
-                    var invisible = AsteroidAt(cur);
-                    if (invisible.HasValue)
-                        visible.Remove(invisible.Value);
-                    cur += step;
-                }
+        public bool IsVisible(IVec2 source, IVec2 target)
+        {
+            return Line(source, target)
+                .Skip(1) // we might start on an asteroid
+                .All(v => AsteroidAt(v) == null);
+        }
+
+        public IVec2 BestCoordinates() => asteroids.OrderByDescending(CountVisibleAsteroids).First();
+        public int BestCount() => asteroids.Max(CountVisibleAsteroids);
+        public int CountVisibleAsteroids(IVec2 source) => asteroids.Except(new[] { source }).Count(t => IsVisible(source, t));
+
+        public IEnumerable<IVec2> VaporizeAsteroids(IVec2 source)
+        {
+            var asteroidsLeft = asteroids.Except(new[] { source }).Select(a => a - source).ToArray();
+            while (asteroidsLeft.Any())
+            {
+                var nextRound = asteroidsLeft
+                    .GroupBy(v => v.Angle)
+                    .OrderBy(v => v.Key < 0.0f ? 2 * Math.PI + v.Key : v.Key)
+                    .Select(vg => vg.OrderBy(v => v.LengthSqr).First())
+                    .ToArray();
+                foreach (var nr in nextRound)
+                    yield return source + nr;
+
+                asteroidsLeft = asteroidsLeft.Except(nextRound).ToArray();
             }
-            return visible;
         }
 
-        public int BestIndex()
-        {
-            return Enumerable
-                .Range(0, asteroids.Length)
-                .Select(i => (index: i, visible: VisibleFrom(i).ToArray()))
-                .OrderByDescending(p => p.visible.Length)
-                .First().index;
-        }
-
-        public Vector2Int BestCoordinates()
-        {
-            Vector2 c = asteroids[BestIndex()];
-            return new Vector2Int((int)c.x, (int)c.y);
-        }
-
-        static void Assert<T>(T expected, T actual)
+        static void Assert<T>(T expected, T actual, string id = "")
         {
             if (!expected.Equals(actual))
-                throw new InvalidProgramException("Assertion failed");
+                Console.WriteLine($"ASSERTION FAILED {id}: {expected} != {actual}");
         }
+
+        private const string CommonExample = @"
+#.........
+...#......
+...#..a...
+.####....a
+..#.c.b...
+.....c....
+..efd.c.gb
+.......c..
+....f...c.
+...e..d..c";
+
+        private const string CommonExample2 = @"
+.#..#
+.....
+#####
+....#
+...##";
+
+        private const string CommonExample3 = @"
+.#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....X...###..
+..#.#.....#....##";
+        private static readonly IVec2 XFor3Pos = new IVec2(8, 3);
+        private static readonly IVec2 XFor4Pos = new IVec2(11, 13);
+
+        private const string CommonExample4=@"
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##";
+
 
         static void tests()
         {
-            Assert(new Vector2Int(3, 4), new AsteroidMap(@"
-                .#..#
-                .....
-                #####
-                ....#
-                ...##").BestCoordinates());
+            Assert(true, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(3, 1)), "0");
+            Assert(true, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(3, 2)), "1");
+            Assert(true, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(1, 3)), "2");
+            Assert(true, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(2, 3)), "3");
+            Assert(true, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(3, 3)), "4");
+            Assert(true, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(4, 3)), "5");
+            Assert(true, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(2, 4)), "6");
+            Assert(false, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(6, 2)), "7");
+            Assert(false, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(9, 3)), "8");
+            Assert(false, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(4, 4)), "9");
+            Assert(false, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(5, 5)), "a");
+            Assert(false, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(6, 6)), "b");
+            Assert(false, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(6, 4)), "c");
+            Assert(false, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(9, 6)), "d");
+            Assert(false, new AsteroidMap(CommonExample).IsVisible(IVec2.Zero, new IVec2(8, 6)), "e");
 
-            Assert(new Vector2Int(5, 8), new AsteroidMap(@"
+            Assert(7, new AsteroidMap(CommonExample2).CountVisibleAsteroids(new IVec2(1, 0)), "f");
+            Assert(7, new AsteroidMap(CommonExample2).CountVisibleAsteroids(new IVec2(4, 0)), "g");
+            Assert(6, new AsteroidMap(CommonExample2).CountVisibleAsteroids(new IVec2(0, 2)), "h");
+            Assert(7, new AsteroidMap(CommonExample2).CountVisibleAsteroids(new IVec2(1, 2)), "i");
+            Assert(7, new AsteroidMap(CommonExample2).CountVisibleAsteroids(new IVec2(2, 2)), "j");
+            Assert(7, new AsteroidMap(CommonExample2).CountVisibleAsteroids(new IVec2(3, 2)), "k");
+            Assert(5, new AsteroidMap(CommonExample2).CountVisibleAsteroids(new IVec2(4, 2)), "l");
+            Assert(7, new AsteroidMap(CommonExample2).CountVisibleAsteroids(new IVec2(4, 3)), "m");
+            Assert(7, new AsteroidMap(CommonExample2).CountVisibleAsteroids(new IVec2(4, 4)), "n");
+            Assert(8, new AsteroidMap(CommonExample2).CountVisibleAsteroids(new IVec2(3, 4)), "o");
+
+            Assert(new IVec2(3, 4), new AsteroidMap(CommonExample2).BestCoordinates());
+
+            Assert(new IVec2(5, 8), new AsteroidMap(@"
                 ......#.#.
                 #..#.#....
                 ..#######.
@@ -108,7 +183,7 @@ namespace day10
                 ##...#..#.
                 .#....####").BestCoordinates());
 
-            Assert(new Vector2Int(1, 2), new AsteroidMap(@"
+            Assert(new IVec2(1, 2), new AsteroidMap(@"
                 #.#...#.#.
                 .###....#.
                 .#....#...
@@ -120,7 +195,7 @@ namespace day10
                 ......#...
                 .####.###.").BestCoordinates());
 
-            Assert(new Vector2Int(6, 3), new AsteroidMap(@"
+            Assert(new IVec2(6, 3), new AsteroidMap(@"
                 .#..#..###
                 ####.###.#
                 ....###.#.
@@ -132,34 +207,48 @@ namespace day10
                 .##...##.#
                 .....#.#..").BestCoordinates());
 
-            Assert(new Vector2Int(11, 13), new AsteroidMap(@"
-                .#..##.###...#######
-                ##.############..##.
-                .#.######.########.#
-                .###.#######.####.#.
-                #####.##.#.##.###.##
-                ..#####..#.#########
-                ####################
-                #.####....###.#.#.##
-                ##.#################
-                #####.##.###..####..
-                ..######..##.#######
-                ####.##.####...##..#
-                .#####..#.######.###
-                ##...#.##########...
-                #.##########.#######
-                .####.#.###.###.#.##
-                ....##.##.###..#####
-                .#.#.###########.###
-                #.#.#.#####.####.###
-                ###.##.####.##.#..##").BestCoordinates());
+            Assert(new IVec2(11, 13), new AsteroidMap(CommonExample4).BestCoordinates());
+
+            var vaporizeList = new AsteroidMap(CommonExample3).VaporizeAsteroids(XFor3Pos).ToArray();
+            var expectList = new[]
+            {
+                new IVec2(8, 1), new IVec2(9, 0), new IVec2(9, 1), new IVec2(10, 0), new IVec2(9, 2), new IVec2(11, 1), new IVec2(12, 1), new IVec2(11, 2), new IVec2(15, 1),
+                new IVec2(12, 2), new IVec2(13, 2), new IVec2(14, 2), new IVec2(15, 2), new IVec2(12, 3), new IVec2(16, 4), new IVec2(15, 4), new IVec2(10, 4), new IVec2(4, 4),
+                new IVec2(2, 4), new IVec2(2, 3), new IVec2(0, 2), new IVec2(1, 2), new IVec2(0, 1), new IVec2(1, 1), new IVec2(5, 2), new IVec2(1, 0), new IVec2(5, 1),
+                new IVec2(6, 1), new IVec2(6, 0), new IVec2(7,0), new IVec2(8, 0), new IVec2(10, 1), new IVec2(14, 0), new IVec2(16, 1), new IVec2(13, 3), new IVec2(14, 3)
+            };
+            for (int i = 0; i < expectList.Length; i++)
+                Assert(expectList[i], vaporizeList[i], $"V{i/9 + 1}.{(i%9) + 1}");
+
+            var expectMap = new Dictionary<int, IVec2>()
+            {
+                {1, new IVec2(11, 12) },
+                {2, new IVec2(12, 1) },
+                {3, new IVec2(12, 2) },
+                {10, new IVec2(12, 8) },
+                {20, new IVec2(16, 0) },
+                {50, new IVec2(16, 9) },
+                {100, new IVec2(10, 16) },
+                {199, new IVec2(9, 6) },
+                {200, new IVec2(8, 2) },
+                {201, new IVec2(10, 9) },
+                {299, new IVec2(11, 1) }
+            };
+            var vaporizeList2 = new AsteroidMap(CommonExample4).VaporizeAsteroids(XFor4Pos).ToArray();
+            foreach (var pair in expectMap)
+                Assert(pair.Value, vaporizeList2[pair.Key - 1], $"L{pair.Key}");
         }
 
         static void Main(string[] args)
         {
             tests();
-            Console.WriteLine("done");
-            Console.ReadKey(true);
+            Console.WriteLine("done testing");
+
+            var map = new AsteroidMap(File.ReadAllText("input.txt"));
+            Console.WriteLine(map.BestCount());
+
+            var vaporizeList = map.VaporizeAsteroids(map.BestCoordinates()).ToArray();
+            Console.WriteLine(vaporizeList[199]);
         }
     }
 }
